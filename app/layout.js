d726e87ada620d8e01658575e7bf565dce68bd1b
radouan larabi <116183732+radlarabi@users.noreply.github.com>
2024-03-10 14:09:52 +0000

    init next project

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import { Sora } from "next/font/google";
+import "./globals.css";
+
+const sora = Sora({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Brainwave",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={sora.className}>{children}</body>
+    </html>
+  );
+}
